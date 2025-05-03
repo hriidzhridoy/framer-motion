793f82ed06a9cf54838a11f07cc0335b4129b7bd
hriidzhridoy <amamunhridoy@gmail.com>
2025-05-04 00:58:55 +0600

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { AvatarListDemo } from "./components/ui/Avatar";
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center bg-black/190 h-screen">
+      <AvatarListDemo />
+    </div>
+  );
+}
