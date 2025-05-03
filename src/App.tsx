fa3ba36ccdc221c7ab5e65a7bf6399a76b2c86bb
hriidzhridoy <amamunhridoy@gmail.com>
2025-05-04 02:36:16 +0600

    water ripple background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import WaterRippleEffect from "./components/ui/wripple-effect";
 import FirstPage from "./pages/FirstPage";
 import SecondPage from "./pages/SecondPage";
 
 export default function App() {
   return (
     <div>
-      <FirstPage />
+      <div className="relative w-full h-[800px] rounded-lg flex items-center justify-center overflow-hidden">
+        {/* <WaterRippleEffect
+          waveSpeedX={0.02}
+          waveSpeedY={0.01}
+          waveAmpX={50}
+          waveAmpY={10}
+          backgroundColor="transparent"
+          lineColor="rgba(255, 255, 255, 0.3)"
+          style={{
+            backgroundImage: "url(https://img.buoucoding.com/image/17.jpg)",
+            backgroundSize: "cover",
+          }}
+        />
+        <div className="relative z-10 mx-auto px-4 md:px-6 text-center text-white"> */}
+        <FirstPage />
+        {/* </div> */}
+      </div>
+
       <SecondPage />
     </div>
   );
